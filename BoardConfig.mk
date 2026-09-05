cef48b94b0c6d17aec2b7c614022e42c6524415e
naosil <natstonsilalahi@gmail.com>
2026-09-05 18:43:38 +0700

    A37f: Use TARGET_USES_64_BIT_BINDER Flag

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -33,6 +33,9 @@ TARGET_PLATFORM_DEVICE_BASE := /devices/soc.0/
 TARGET_BOOTLOADER_BOARD_NAME := MSM8916
 TARGET_NO_BOOTLOADER := true
 
+# Binder API version
+TARGET_USES_64_BIT_BINDER := true
+
 # Kernel
 BOARD_KERNEL_BASE := 0x80000000
 BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 androidboot.hardware=qcom msm_rtb.filter=0x237 ehci-hcd.park=3 androidboot.bootdevice=7824900.sdhci lpm_levels.sleep_disabled=1 androidboot.selinux=permissive
